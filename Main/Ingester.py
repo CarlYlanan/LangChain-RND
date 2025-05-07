d578b8d2c052f4d26339acae18aa7cb7d9306a1f
NikishaChhima <npchhima@gmail.com>
2025-05-07 20:27:20 +1200

    Changing PDF loader to utilise LangChain

--- Main/Ingester.py
+++ Main/Ingester.py
@@ -1,13 +1,11 @@
 #Importing PdfReader to read sample documents
-from PyPDF2 import PdfReader
+from langchain.document_loaders import PyPDFLoader
 
 #Providing path to pdf file
-reader1 = PdfReader("../sample_documents/sample1.pdf")
+loader1 = PyPDFLoader("../sample_documents/sample1.pdf")
+pages = loader1.load()
 
 #Loop through all pages within pdf and printing out its context
-for page_num in range(len(reader1.pages)):
-    #Extract info from each page 
-    page = reader1.pages[page_num]
-    
-    #Printing text within pdf file to terminal for now
-    print(page.extract_text())
+for page in pages:
+    #Printing content within pdf file to terminal for now
+    print(page.page_content)
