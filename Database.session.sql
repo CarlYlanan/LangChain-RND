a5473b0bb35752ebefd6c5adb45cfb5098e11b24
NikishaChhima <npchhima@gmail.com>
2025-08-30 14:39:03 +1200

    Fixing table to accept null for certain fields

--- Database.session.sql
+++ Database.session.sql
@@ -1,14 +1,11 @@
 CREATE TABLE IF NOT EXISTS referral_triage_results (
     patient_id SERIAL PRIMARY KEY,
     patient_name TEXT NOT NULL,
-    dob DATE NOT NULL,
-    nhs_number TEXT NOT NULL,
-    hospital_id TEXT NOT NULL,
-    fake_name TEXT NOT NULL,
     source_file TEXT NOT NULL,
-    result TEXT NOT NULL,
-    rationale TEXT NOT NULL
+    dob DATE,
+    nhs_number TEXT,
+    hospital_id TEXT,
+    fake_name TEXT DEFAULT '',
+    result TEXT DEFAULT 'Pending Triage',
+    rationale TEXT DEFAULT ''
 );
-
-
-
