9b6ac47167c1513ba18545aa9b556c924fb2c2e8
Peter Guilbert <no.8wire@hotmail.com>
2025-09-08 16:05:55 +1200

    Added back feedback prompts in main, added 5 new pdf referral letters.

--- Main_version_2/Main.py
+++ Main_version_2/Main.py
@@ -149,8 +149,17 @@ if __name__ == "__main__":
         # Passing information to our AI triaging algorithm
         #print("\nAI Triage Output:")
         ai_triage_output = ai_triage(semi_unstructured_text, file_name, dob=dob)
-        #print(ai_triage_output)
+        print(ai_triage_output)
 
+        #feedback=input("Enter feedback here (or press Enter if decision was correct):")
+        #final_result=input("Enter final result (Priority X/Not Accepted, or press Enter if same as AI):")
+
+        #if feedback.strip() or final_result.strip():
+            #accepting_feedback(file_name, ai_triage_output, feedback or "No feedback", final_result or ai_triage_output)
+        #else:
+            #print("Acknowledged. No feedback saved.")                
+            
+        
     # Stopping timer for performance metrics
     end_time = time.time()
     print(f"Total execution time: {end_time - start_time:.2f} seconds")
